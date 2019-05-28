1f478f9b1befe0052d6df4edcac5774a9efd7376
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-28 15:34:42 -0700

    created basic express server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+// set a port for the server
+const port = "4000"; 
+// import the db (data manager file)
+const db = require("./data/db");
+// npm module, needs to be installed, equivalent to import express from 'express';
+const express = require("express");
+// creates an http web server
+const server = express(); 
+// makes the web server listen for incoming traffic on port 4000
+server.listen(4000, () => {
+    //this callback function runs after the server starts successfully
+    console.log('\n*** Server Running on http://localhost:4000 ***\n');
+});
+
+
+
+
