78c744f04cec2082b1f9d91cb29b11623113b1ce
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-29 03:02:42 -0700

    added post

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.listen(4000, () => {
 
 //get all users
 server.get('/api/users', (req, res) => {
-// db.find() returns a promise that resolves to a list of existing hubs  
+// db.find() returns a promise that resolves to a list of existing users  
     db.find()
         .then(users => { 
              res
@@ -31,5 +31,58 @@ server.get('/api/users', (req, res) => {
             });
         });
 });
+
+
+//get all users
+server.get('/api/users', (req, res) => {
+    // db.find() returns a promise that resolves to a list of existing hubs  
+        db.find()
+            .then(users => { 
+                 res
+                 .status(200)
+                 .json(users);
+            })
+            .catch(err => {
+            // we ran into an error getting the users
+            // use the catch-all 500 status code
+                res
+                .status(500)
+                .json({
+                    error: "The users information could not be retrieved." 
+                });
+            });
+    });
+
+
+//post to users
+
+server.post('/api/users', (req, res) => {
+    // one way a client can send information is in the request body
+    // axios.post(url, payload) << the payload will show up as req.body on the server
+        const user = req.body;
+        if(!user.name || !user.bio) {
+            return res
+            .status(400)
+            .json({
+                error: "Please provide name and bio for the user."
+            })
+        }
+        db
+            .add({ user })
+            .then(users => { 
+                 res
+                 .status(200)
+                 .json(users); //this is the payload
+            })
+            .catch(err => {
+            // we ran into an error getting the users
+            // use the catch-all 500 status code
+                res
+                .status(500)
+                .json({
+                    error: "There was an error while saving the user to the database" 
+                });
+            });
+    });
 
 
